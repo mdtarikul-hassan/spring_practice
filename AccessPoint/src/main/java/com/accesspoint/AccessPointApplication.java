08b4ba2cb7b948a31a2f333285d0228b53c5818a
tarikul <mdtarikulhassan5432@gmail.com>
2026-04-09 11:51:18 +0530

    AccessPoint : create a new full stack authentication project using spring boot and react vite

---
+++ AccessPoint/src/main/java/com/accesspoint/AccessPointApplication.java
@@ -0,0 +1,13 @@
+package com.accesspoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessPointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessPointApplication.class, args);
+    }
+
+}
